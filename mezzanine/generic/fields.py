fd0086aea5608a43e34dce50bc49894d78bb9947
strato <clapama1@gmail.com>
2019-08-19 20:40:55 -0400

    fix to 'Direct assignment to the reverse side of a related set is prohibited. Use +.set() instead. '

--- mezzanine/generic/fields.py
+++ mezzanine/generic/fields.py
@@ -203,6 +203,9 @@ class KeywordsField(BaseGenericRelation):
             data = [related_manager.create(keyword_id=i) for i in new_ids]
         # Remove keywords that are no longer assigned to anything.
         Keyword.objects.delete_unused(removed_ids)
+        # Return from object if foreign key
+        if type(data) == list:
+            return
         super(KeywordsField, self).save_form_data(instance, data)
 
     def contribute_to_class(self, cls, name):
